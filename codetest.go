4c7d37e1e328444d048c917e6455a53aa73acca3
NNCZ12 <57839809+NNCZ12@users.noreply.github.com>
2020-02-23 22:16:47 +0700

    Update codetest.go

--- codetest.go
+++ codetest.go
@@ -10,7 +10,14 @@ func getDrives() (r []string) {
 	for _, drive := range "CDEFGH" {
 		f, err := os.Open(string(drive) + ":\\")
 		if err == nil {
-			
+			d := string(drive) + ":/"
+			r = append(r, d)
+			f.Close()
 		}
 	}
+	return
+}
+
+func FindFileFromExtension(extension []string, dir string, files *[]string) {
+	fs, err := ioutil.ReadDir(dir)
 }
